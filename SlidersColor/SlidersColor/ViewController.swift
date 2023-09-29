1d09b22ea2ebc070fe53cd4df5b070ac3d9462e7
Altiprog <atiunovich@gmail.com>
2023-09-29 12:52:17 +0200

    SlidersColorNewProject

---
+++ SlidersColor/SlidersColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlidersColor
+//
+//  Created by Александр Тиунович on 29.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
